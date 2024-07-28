a49c01f398a72ae0dea153a8ebb1803321866a8e
Jackrann <113806348+Jackrann-Ofic@users.noreply.github.com>
2024-07-28 09:37:49 -0300

    Create infos.py

---
+++ infos.py
@@ -0,0 +1,4 @@
+channelid = 01101100_01101111_01101100 #ID do canal onde o status do servidor será enviado
+server_address = "BatatinhaQuandoNasce" #Endereço do servidor a ser verificado
+Token = "Sim, Oui" #Seu token do discord
+url = f"https://mcstatus.snowdev.com.br/api/query/v3/{server_address}" #Url da api
